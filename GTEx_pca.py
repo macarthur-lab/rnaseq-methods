88a0c4cbc0f52a9561060b7cfcc23d36618a9b3f
Jorge Perez de Acha Chavez <jperezde@broadinstitute.org>
2019-02-01 14:53:17 +0000

    Removed mean-centering after normalization

--- GTEx_pca.py
+++ GTEx_pca.py
@@ -6,7 +6,7 @@ from sklearn.decomposition import PCA
 import pickle 
 import datetime
 
-def run_pca(file, transpose=True, tissues = None, tissues_of_interest=None, log_transform = True, scaler = 'standard', n_components = None, save_pca = True, pca_name = "GTEx_pca"):
+def run_pca(file, transpose=True, tissues = None, tissues_of_interest=None, log_transform = True, scaling = 'standard', n_components = None, save_pca = True, pca_name = "GTEx_pca"):
 	""" Given a set of gene expression values, run an exploratory Principal Component Analysis (PCA) and store the loadings.
 
 	:param file (str):			file path of tab separated gene expression values
@@ -14,7 +14,7 @@ def run_pca(file, transpose=True, tissues = None, tissues_of_interest=None, log_
 	:param tissues (str):			file path of tissues corresponding to each sample [id, tissue, subtissue] (optional)
 	:param tissues_of_interest (str array):	tissues in which to perform PCA (optional)
 	:param log_transform (bool):		perform log2(gene_expression + 1) transformation (default True)
-	:param scaler (str):			data scaling method. If 'None', data is not scaled. Accepted scalers 'standard', 'robust', 'normalizer' (default 'standard')
+	:param scaling (str):			data scaling method. If 'None', data is not scaled. Accepted scalers 'standard', 'robust', 'normalizer' (default 'standard')
 	:param n_components (int):		number of principal components to store. If 'None', all are stored (default None)
 	:param save_pca (bool):			store sklearn.decomposition.PCA object fitted to data as pickle object (default True) 			
 	:param pca_name (str):			basename of files to be stored (default 'GTEx_pca')
@@ -52,28 +52,25 @@ def run_pca(file, transpose=True, tissues = None, tissues_of_interest=None, log_
 		print(now() + ": Performing log2 transformation")
 		tpms = np.log2(tpms + 1)
 
-	if scaler is not None:
-		if scaler == 'standard':
+	if scaling is not None:
+		if scaling == 'standard':
 			# (x_i - mean(X))/stdev(X)
 			scaler = StandardScaler()
-		elif scaler == 'robust':
+		elif scaling == 'robust':
 			# (x_i - Q_1(X))/(Q_3(X) - Q_1(X))
 			scaler = RobustScaler()
-		elif scaler == 'normalizer':
-			mean_scaler = StandardScaler(with_mean = True, with_std = False)
-			mean_scaler.fit(tpms)
-			tpms = mean_scaler.transform(tpms)
+		elif scaling == 'normalizer':
 
 			# x_i / sqrt(x_i, y_i, z_i, ...)
 			scaler = Normalizer()
 		else:
-			print(scaler + " not recognized. Using standard scaler.")
+			print(scaling + " not recognized. Using standard scaler.")
 			scaler = StandardScaler()
 
 		print(now() + ": Scaling data")
 		scaler.fit(tpms)
 		tpms = scaler.transform(tpms)
-	
+
 	print(now() + ": Performing PCA")
 	pca = PCA(n_components=n_components, random_state = 42, svd_solver = 'full')
 	pca.fit(tpms)
@@ -81,9 +78,9 @@ def run_pca(file, transpose=True, tissues = None, tissues_of_interest=None, log_
 	if save_pca:
 		print(now() + ": Saving PCA object")
 		file_name = str(pca_name) + ".object"
-		open(file_name, 'wb')
-		pickle.dump(pca, file_name, protocol=4)
-		file_name.close()
+		#open(file_name, 'wb')
+		pickle.dump(pca, open(file_name, 'wb'), protocol=4)
+		# file_name.close()
 	
 	print(now() + ": Transforming principal components")
 	pcs = pca.transform(tpms)
