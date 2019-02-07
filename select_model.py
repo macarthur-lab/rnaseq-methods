742afffcfcb2492840272fd7fae4526d0a2b32d3
Jorge Perez de Acha Chavez <jperezde@broadinstitute.org>
2019-02-07 10:58:38 -0500

    Added logistic regression, linear SVC. Code cleanup

--- select_model.py
+++ select_model.py
@@ -2,16 +2,21 @@ import sys
 import functools
 from GTEx_pca import *
 from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler, RobustScaler, Normalizer
-# from sklearn.decomposition import PCA
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 print = functools.partial(print, flush=True)
 
+classifiers_to_use = {
+    'gnb': {'classifier': GaussianNB(), 'acc'=[]},
+    'knn': {'classifier': KNeighborsClassifier(n_neighbors=7), 'acc'=[]},
+    'lr': {'classifier': LogisticRegression(randome_state = 42, solver = 'lbfgs', multi_class='ovr', n_jobs = -1), 'acc' = []},
+    'lin_svc': {'classifier': LinearSVC(multi_class='ovr', random_state = 42, max_iter = 2500), 'acc' = []}
+}
 
 # Naive bayes
 # https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_bayes.GaussianNB
@@ -19,8 +24,12 @@ print = functools.partial(print, flush=True)
 # https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
 # Random forest
 # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier
+# Logistic regression
+# https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#sklearn.linear_model.LogisticRegression
 # Support vector machine classification
 # https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
+# Linear Support Vector Classification
+# https://scikit-learn.org/stable/modules/generated/sklearn.svm.LinearSVC.html#sklearn.svm.LinearSVC
 
 # Load data
 print(now() + ": Loading data")
@@ -39,7 +48,7 @@ print(now() + ": Splitting into training and test sets")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .20, random_state = 42, stratify = y)
 
 # Get pca and scaler fitted to training set
-pca, scaler = run_pca(data = X_train, transpose = False, log_transform = False, scaling = "normalizer", n_components = 500, save_pca = True, save_scaler = True, pca_name = "training_pca")
+pca, scaler = run_pca(data = X_train, transpose = False, log_transform = False, scaling = "normalizer", n_components = 500, save_pca = True, save_scaler = True, pca_name = "training_pca_2")
 
 print(now() + ": Normalizing sets")
 X_train = scaler.transform(X_train)
@@ -49,58 +58,24 @@ print(now() + ": Transforming into principal components")
 X_train = pca.transform(X_train)
 X_test = pca.transform(X_test)
 
-gnb_acc = []
-knn_acc = []
-rf_acc = []
-svc_rbf_acc = []
-svc_sig_acc = []
+pcs = [i for i in range(5,41) if (i % 5 == 0 or i % 5 == 3)]
 
-for i in range(5, 501, 5):
-	print(now() + ": Obtaining accuracies with " + str(i) + " principal components")
-	X_train_slice = X_train[:,:i]
-	X_test_slice = X_test[:,:i]
+for i in pcs:
+    print(now() + ": Obtaining accuracies with " + str(i) + " principal components")
+    X_train_slice = X_train[:,:i]
+    X_test_slice = X_test[:,:i]
 
-	# Naive Bayes
-	print(now() + ":    Naive bayes")
-	gnb = GaussianNB()
-	gnb.fit(X_train_slice, y_train)
-	y_pred = gnb.predict(X_test_slice)
-	gnb_acc.append(accuracy_score(y_test, y_pred))
-
-	# K-Nearest Neighbors
-	print(now() + ":    K-Nearest neighbors")
-	knn = KNeighborsClassifier(n_neighbors=6)
-	knn.fit(X_train_slice, y_train)
-	y_pred = knn.predict(X_test_slice)
-	knn_acc.append(accuracy_score(y_test, y_pred))
-
-	# Random Forest
-	print(now() + ":    Random forest")
-	rf = RandomForestClassifier(max_depth=10, random_state=42, bootstrap=100)
-	rf.fit(X_train_slice, y_train)
-	y_pred = rf.predict(X_test_slice)
-	rf_acc.append(accuracy_score(y_test, y_pred))
-
-	# SVM classification with radial basis function kernel
-	print(now() + ":    SVMC with rbf kernel")
-	svc = SVC(kernel = 'rbf', random_state = 42)
-	svc.fit(X_train_slice, y_train)
-	y_pred = svc.predict(X_test_slice)
-	svc_rbf_acc.append(accuracy_score(y_test, y_pred))
-	
-	# SVM classification with sigmoid kernel
-	print(now() + ":    SVMC with sigmoid kernel")
-	svc = SVC(kernel = 'sigmoid', random_state = 42)
-	svc.fit(X_train_slice, y_train)
-	y_pred = svc.predict(X_test_slice)
-	svc_sig_acc.append(accuracy_score(y_test, y_pred))
+    for method in classifiers_to_use:
+        print(now() + ": " + method)
+        classifiers_to_use[method]['acc'].append(get_accuracy(classifiers_to_use[method]['classifier'], X_train_slice, y_train, X_test_slice, y_test))
 	
 print(now() + ": Saving accuracy scores")
-np.savetxt('gnb_acc',np.asarray(gnb_acc))
-np.savetxt('knn_acc',np.asarray(knn_acc))
-np.savetxt('rf_acc',np.asarray(rf_acc))
-np.savetxt('svc_rbf_acc',np.asarray(svc_rbf_acc))
-np.savetxt('svc_sig_acc',np.asarray(svc_sig_acc))
+for method in classifiers_to_use:
+    np.savetxt(method, np.asarray(classifiers_to_use[method]['acc']))
 
+def get_accuracy(classifier, X_train, y_train, X_test, y_test):
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    return accuracy_score(y_test, y_pred)
 
 
