16d4c19af3733d99f1ed5cdfbfaa88e9bc426fd4
Jorge Perez de Acha Chavez <jperezde@broadinstitute.org>
2019-03-19 14:13:53 +0000

    Several minor fixes

--- compare_patient_to_model.py
+++ compare_patient_to_model.py
@@ -1,12 +1,12 @@
-from model import *
+from model_funcs import *
 
 splice_file = '/home/jperezde/work/NEB_prop_model/153BR_JB_M1.splicing.txt'
 model_file = '/home/jperezde/work/NEB_prop_model/NEB_model_no_annotation_filtered'
 
 with open(model_file) as f:
-    model_splices = f.readline()
+    model_splices = f.readline().rstrip('\n').split(' ')
 
-model_splices = np.fromstring(model_splices, dtype='str', sep=' ')
+model_splices = np.array(model_splices) 
 
 model_prop = np.loadtxt(model_file, delimiter=' ', skiprows=1)
 num_controls = len(model_prop[:,0])
@@ -20,21 +20,21 @@ comparison_model = np.zeros((num_controls, len_comparison_model))
 
 for i in range(0, len(joined_splices)):
     key = joined_splices[i]
-    patient_idx = np.where(patient_splices == key)
-    model_idx = np.where(model_splices == key)
+    patient_idx = np.where(patient_splices == key)[0]
+    model_idx = np.where(model_splices == key)[0]
 
-        if len(patient_idx) == 0:
-            # splice is not found in patient
-            comparison_model[i] = model_prop[:,model_idx]
-            patient_splices = np.append(patient_splices, key)
-            patient_prop = np.append(patient_prop, 0)
+    if len(patient_idx) == 0:
+        # splice is not found in patient
+        comparison_model[:,i] = model_prop[:,model_idx[0]]
+        patient_splices = np.append(patient_splices, key)
+        patient_prop = np.append(patient_prop, 0)
 
-        elif len(patient_idx) != 0 and len(model_idx) != 0
-        # splice site in patient and model
-            comparison_model[i] = model_prop[:,model_idx]
+    elif len(patient_idx) != 0 and len(model_idx) != 0:
+    # splice site in patient and model
+        comparison_model[:,i] = model_prop[:,model_idx[0]]
 
 patient_splices = ' '.join(patient_splices)
 joined_splices = ' '.join(joined_splices)
 
 np.savetxt('NEB_comparison_model', comparison_model, delimiter=' ', header=patient_splices)
-np.savetxt('NEB_patient_model', patient_prop, delimiter=' ', header=joined_splices)
+np.savetxt('NEB_patient_model', patient_prop.reshape(1, len(patient_prop)), delimiter=' ', header=joined_splices)
