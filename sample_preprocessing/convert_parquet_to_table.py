a30239b51d1760249aeafc6db537a5e26d1ec507
bw2 <weisburd@broadinstitute.org>
2019-12-04 15:08:26 -0500

    convert_parquet_to_table.py

--- sample_preprocessing/convert_parquet_to_table.py
+++ sample_preprocessing/convert_parquet_to_table.py
@@ -8,32 +8,20 @@ def parse_args():
     p.add_argument("path", help="Path of .parquet file")
     return p.parse_args()
 
-"""
-COLUMNS_TO_OUTPUT = [
-    "chrom",
-    "start_1based",
-    "end_1based",
-    "strand",
-    "intron_motif",
-    "known_splice_junction",
-    "unique_reads",
-    "multi_mapped_reads",
-    "maximum_overhang",
-]
-"""
 
 def main():
     args = parse_args()
 
     df = pd.read_parquet(args.path).reset_index()
 
-    ## write as tsv
     output_prefix = args.path.replace(".parquet", "")
     if args.normalize_read_counts:
         output_prefix += ".normalized_counts"
 
     df.to_csv(f"{output_prefix}.with_header.tab", header=True, sep="\t", index=False)
     df.to_csv(f"{output_prefix}.tab", header=False, sep="\t", index=False)
+
+    print(f"Converted {args.path} out {output_prefix}.tab")
 
 
 if __name__ == "__main__":
