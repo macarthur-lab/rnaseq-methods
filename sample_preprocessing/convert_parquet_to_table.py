1c6424179770eb772f0b81ec304dc6185027cd69
bw2 <weisburd@broadinstitute.org>
2019-12-04 14:39:21 -0500

    fixed

--- sample_preprocessing/convert_parquet_to_table.py
+++ sample_preprocessing/convert_parquet_to_table.py
@@ -8,7 +8,7 @@ def parse_args():
     p.add_argument("path", help="Path of .parquet file")
     return p.parse_args()
 
-
+"""
 COLUMNS_TO_OUTPUT = [
     "chrom",
     "start_1based",
@@ -20,22 +20,20 @@ COLUMNS_TO_OUTPUT = [
     "multi_mapped_reads",
     "maximum_overhang",
 ]
-
+"""
 
 def main():
     args = parse_args()
 
-    df = pd.read_parquet(args.path)
-
-    num_samples = len([c for c in df.columns if c.startswith("unique_reads_")])
+    df = pd.read_parquet(args.path).reset_index()
 
     ## write as tsv
-    output_prefix = f"combined.{num_samples}_samples"
+    output_prefix = args.path.replace(".parquet", "")
     if args.normalize_read_counts:
         output_prefix += ".normalized_counts"
 
-    df[COLUMNS_TO_OUTPUT].to_csv(f"{output_prefix}.with_header.SJ.out.tab", header=True, sep="\t", index=False)
-    df[COLUMNS_TO_OUTPUT].to_csv(f"{output_prefix}.SJ.out.tab", header=False, sep="\t", index=False)
+    df.to_csv(f"{output_prefix}.with_header.tab", header=True, sep="\t", index=False)
+    df.to_csv(f"{output_prefix}.tab", header=False, sep="\t", index=False)
 
 
 if __name__ == "__main__":
