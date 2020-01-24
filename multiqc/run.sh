b7670ff4bc96b50622236d8e356f557c47747460
bw2 <weisburd@broadinstitute.org>
2020-01-23 22:55:17 -0500

    generate both static and interactive plots

--- multiqc/run.sh
+++ multiqc/run.sh
@@ -1,4 +1,5 @@
-multiqc -f -m star -m fastqc -m rna_seqc --filename all.html . 
+multiqc -f -m star -m fastqc -m rna_seqc --filename all2.html . 
+multiqc -f -m star -m fastqc -m rna_seqc --filename all.html -ip . 
 #multiqc -f -m rna_seqc --filename all.html . 
 #for sample_list in *_list.txt; do 
 #    multiqc -f -m star -m fastqc --filename $(echo $sample_list | sed 's/_list.txt//').html --file-list $sample_list . 
