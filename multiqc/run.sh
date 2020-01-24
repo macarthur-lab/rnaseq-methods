c6c049e5aad0edd65c7afdd768a085dcc46c2acc
bw2 <weisburd@broadinstitute.org>
2020-01-23 23:21:36 -0500

    run.sh

--- multiqc/run.sh
+++ multiqc/run.sh
@@ -1,4 +1,5 @@
-multiqc -f -m star -m fastqc -m rna_seqc --filename all2.html . 
+multiqc -f -m star -m fastqc -m rna_seqc --filename all.html . 
+mv all.html all2.html
 multiqc -f -m star -m fastqc -m rna_seqc --filename all.html -ip . 
 #multiqc -f -m rna_seqc --filename all.html . 
 #for sample_list in *_list.txt; do 
