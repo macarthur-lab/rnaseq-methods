bd81f4dbb35b5e30762858ccae6cf69dbd191d78
bw2 <weisburd@broadinstitute.org>
2020-04-09 10:52:46 -0400

    remove outdated files

--- multiqc/run.sh
+++
@@ -1,7 +0,0 @@
-multiqc -f -m star -m fastqc -m rna_seqc --filename all.html . 
-mv all.html all2.html
-multiqc -f -m star -m fastqc -m rna_seqc --filename all.html -ip . 
-#multiqc -f -m rna_seqc --filename all.html . 
-#for sample_list in *_list.txt; do 
-#    multiqc -f -m star -m fastqc --filename $(echo $sample_list | sed 's/_list.txt//').html --file-list $sample_list . 
-#done
