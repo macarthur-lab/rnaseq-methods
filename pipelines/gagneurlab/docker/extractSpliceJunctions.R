4dc10baa3fc324c42889b73b11bebadf47edb7ab
bw2 <weisburd@broadinstitute.org>
2020-06-01 18:45:47 -0400

    added bam file header, fixed paths

--- pipelines/gagneurlab/docker/extractSpliceJunctions.R
+++ pipelines/gagneurlab/docker/extractSpliceJunctions.R
@@ -2,16 +2,23 @@ library(FRASER)
 library(data.table)
 library(stringr)
 library(purrr)
+library(argparse)
 
-file_paths = list.files('.', pattern = "fraser_count_rna.*.tar.gz$")
+parser = ArgumentParser()
+parser$add_argument("bam_header_path", help="Bam file for getting chromosome names")
+args = parser$parse_args()
+
+file_paths = list.files('.', pattern = "fraser_count_split_reads_.*.tar.gz$")
 print(file_paths)
-parse_sample_id = function(x) { return( str_replace(x[[1]], 'fraser_count_rna_', '')) }
+parse_sample_id = function(x) { return( str_replace(x[[1]], 'fraser_count_split_reads_', '')) }
 sample_ids = unlist(map(strsplit(file_paths, '[.]'), parse_sample_id))
 
-sampleTable = data.table(sampleID=sample_ids, bamFile=".")
+sampleTable = data.table(sampleID=sample_ids, bamFile=args$bam_header_path)
 print(sampleTable)
 
 fds = FraserDataSet(colData=sampleTable, workingDir=".", bamParam=ScanBamParam(mapqFilter=0), strandSpecific=0L)
 splitCountsForAllSamples = getSplitReadCountsForAllSamples(fds)
 splitCountRanges = rowRanges(splitCountsForAllSamples)
+print(splitCountRanges)
+
 saveRDS(splitCountRanges, "spliceJunctions.RDS")
