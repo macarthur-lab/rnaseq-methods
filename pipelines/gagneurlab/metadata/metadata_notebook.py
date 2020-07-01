dd8f1e6abfec731e8a30a82ec668674cfe5624a0
bw2 <weisburd@broadinstitute.org>
2020-07-01 17:57:47 -0400

    add star pipeline batch

--- pipelines/gagneurlab/metadata/metadata_notebook.py
+++ pipelines/gagneurlab/metadata/metadata_notebook.py
@@ -28,18 +28,24 @@ df[['proj (seqr)', 'imputed tissue']].reset_index().groupby(('imputed tissue')).
 
 #%%
 
-df = df[['proj (seqr)', 'imputed tissue', 'imputed sex', 'read length (rnaseqc)', 'batch_date_from_hg19_bam_header']].reset_index()
+df = df[['proj (seqr)', 'imputed tissue', 'imputed sex', 'read length (rnaseqc)', 'batch_date_from_hg19_bam_header', 'star_pipeline_batch']].reset_index()
 
 #%%
 
 df = df[(df['imputed tissue'].str.len() > 2) & (df['batch_date_from_hg19_bam_header'].str.len() > 2)]
 #%%
 
+df = df[df['star_pipeline_batch'] != 'batch_1_muntoni']
+
+#%%
+len(df)
+#%%
 #df['year'] = df['batch_date_from_hg19_bam_header'].apply(lambda x: None if not x.strip() else int(x.split("-")[0]) > 2017)
 
 #%%
 
-df.groupby(['imputed tissue', 'imputed sex', 'read length (rnaseqc)']).count()
+for _, r in df.groupby(['imputed tissue', 'imputed sex', 'read length (rnaseqc)'])['sample_id'].apply(list).reset_index().iterrows():
+    print(r['imputed tissue'], r['imputed sex'], r['read length (rnaseqc)'], r.sample_id)
 
 #%%
 
