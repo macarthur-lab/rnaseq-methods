e2680ef0ecf94202dfe943fafbc2d74397c5c782
zorro3 <zorro3.github@gmail.com>
2020-08-25 02:33:33 -0400

    fixing output

--- pipelines/tgg_viewer/combine_splice_junctions_using_pandas.py
+++ pipelines/tgg_viewer/combine_splice_junctions_using_pandas.py
@@ -129,7 +129,8 @@ def main():
                 scalar = average_unique_reads_per_sample / float(unique_reads_in_sample)
                 df[f"unique_reads_{i}"] *= scalar / float(len(args.paths))
                 df[f"multi_mapped_reads_{i}"] *= scalar / float(len(args.paths))
-
+                df[f"unique_reads_{i}"] = df[f"unique_reads_{i}"].round(decimals=3)
+                df[f"multi_mapped_reads_{i}"] = df[f"multi_mapped_reads_{i}"].round(decimals=3)
                 logging.info(f"{path} has {int(unique_reads_in_sample)} total unique reads while the "
                     f"per-sample average is {average_unique_reads_per_sample}. Scaling read counts "
                     f"by {scalar} and dividing by {len(args.paths)}")
