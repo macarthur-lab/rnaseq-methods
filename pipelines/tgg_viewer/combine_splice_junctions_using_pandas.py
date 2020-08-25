6de5c5a43fc12a315235c864366b04a177283e92
zorro3 <zorro3.github@gmail.com>
2020-08-25 02:05:02 -0400

    refactored and added comments

--- pipelines/tgg_viewer/combine_splice_junctions_using_pandas.py
+++ pipelines/tgg_viewer/combine_splice_junctions_using_pandas.py
@@ -21,17 +21,19 @@ COLUMN_TYPES = {
     'multi_mapped_reads': 'float32',
     'maximum_overhang': 'int16',
     'num_samples_with_this_junction': 'int32',
+    'num_samples_total': 'int32',
     'strand_counter': 'int32',
 }
 
 
 def parse_args():
-    p = argparse.ArgumentParser()
-    p.add_argument("-b", "--batch-size", help="How many tables to merge at once. Bigger batch sizes use more memory.", type=int, default=50)
-    p.add_argument("-N", "--normalize-read-counts", action="store_true", help="whether to normalize read counts")
-    p.add_argument("-c", "--save-read-counts", action="store_true", help="Export separate parquet files with matrices of per-sample unique- and multi-mapped-read counts")
-    p.add_argument("-o", "--output-path")
-    p.add_argument("paths", nargs="+", help="Paths of 1 or more SJ.out.tab tables")
+    p = argparse.ArgumentParser(description="Combines multiple SJ.out.tab tables into one combined .bed file which can be loaded into TGG-viewer to visualize splice junctions")
+    p.add_argument("-n", "--batch-size", help="How many tables to merge in memory before writing to disk. Bigger batch sizes use more memory.", type=int, default=50)
+    p.add_argument("-m", "--normalize-read-counts", action="store_true", help="whether to normalize unique- and multi-mapped read counts rather than just summing them across input tables")
+    p.add_argument("-o", "--output-path", help="Combined .bed output path")
+    p.add_argument("-t", "--save-individual-tables", action="store_true", help="Also export individual .bed files with additional columns")
+    p.add_argument("-p", "--save-parquet-files", action="store_true", help="Also export parquet files with matrices containing per-sample unique_read and multi-mapped read counts. This allows downstream scripts to use these matrices for other kinds of normalization - such as using geometric mean instead of arithmetic mean")
+    p.add_argument("paths", nargs="+", help="Paths of 1 or more input SJ.out.tab tables")
     return p.parse_args()
 
 
@@ -58,7 +60,7 @@ def print_memory_stats(message="", run_gc=False):
     prev_memory_bytes = memory_bytes
 
 
-def read_table(path, i=None):
+def read_SJ_out_tab(path, i=None):
     df = pd.read_csv(
         path,
         names=[
@@ -74,6 +76,7 @@ def read_table(path, i=None):
         sep='\t')
 
     df['num_samples_with_this_junction'] = np.int32(1)
+    df['num_samples_total'] = np.int32(1)
     df['strand_counter'] = df['strand'].apply(lambda s: 1 if s == 1 else (-1 if s == 2 else 0)).astype('int32')
 
     # print some stats
@@ -97,33 +100,39 @@ def main():
     args = parse_args()
 
     if args.normalize_read_counts:
+        # read all the tables to compute total_unique_reads_across_all_samples and average_unique_reads_per_sample
         total_unique_reads_across_all_samples = 0
         for path in args.paths:
-            df = read_table(path)
+            df = read_SJ_out_tab(path)
             total_unique_reads_across_all_samples += df.unique_reads.sum()
 
         average_unique_reads_per_sample = total_unique_reads_across_all_samples/float(len(args.paths))
 
-    result = pd.DataFrame()
     column_names = [
         'strand', 'intron_motif', 'known_splice_junction', 'unique_reads', 'multi_mapped_reads', 'maximum_overhang',
         'num_samples_with_this_junction', 'strand_counter',
     ]
-    parquet_files = defaultdict(list)
+
     i = 0
+    parquet_file_paths = defaultdict(list)
+
+    result = pd.DataFrame()
     logging.info(f"Processing {len(args.paths)} tables")
     for batch_number, batch in enumerate(batched_iter(args.paths, args.batch_size)):
         tables_in_batch = []
         batch_start_i = i
         for path in batch:
             logging.info(f"Batch {batch_number}, Table {i}: {path}")
-            df = read_table(path, i)
+            df = read_SJ_out_tab(path, i)
             if args.normalize_read_counts:
                 unique_reads_in_sample = df[f"unique_reads_{i}"].sum()
                 scalar = average_unique_reads_per_sample / float(unique_reads_in_sample)
                 df[f"unique_reads_{i}"] *= scalar / float(len(args.paths))
                 df[f"multi_mapped_reads_{i}"] *= scalar / float(len(args.paths))
-                logging.info(f"{path} has {int(unique_reads_in_sample)} total unique reads while the per-sample average is {average_unique_reads_per_sample}. Scaling read counts by {scalar} and dividing by {len(args.paths)}")
+
+                logging.info(f"{path} has {int(unique_reads_in_sample)} total unique reads while the "
+                    f"per-sample average is {average_unique_reads_per_sample}. Scaling read counts "
+                    f"by {scalar} and dividing by {len(args.paths)}")
             tables_in_batch.append(df)
             i += 1
         batch_end_i = i
@@ -135,11 +144,13 @@ def main():
             batch_columns[column] = [f'{column}_{k}' for k in range(batch_start_i, batch_end_i)]
             if batch_number > 0: batch_columns[column].append(column)
 
+        # handle intron_motif column
         df = result[batch_columns['intron_motif']]
         df.ffill(axis=1, inplace=True)
         df = df.iloc[:,-1].astype(COLUMN_TYPES['intron_motif'])
         result['intron_motif'] = df
 
+        # handle known_splice_junction column
         df = result[batch_columns['known_splice_junction']]
         df.replace(0, np.nan, inplace=True)
         df.ffill(axis=1, inplace=True)
@@ -152,19 +163,18 @@ def main():
         result['multi_mapped_reads'] = result[batch_columns['multi_mapped_reads']].sum(axis=1).astype(COLUMN_TYPES['multi_mapped_reads'])
         result['maximum_overhang'] = result[batch_columns['maximum_overhang']].max(axis=1).astype(COLUMN_TYPES['maximum_overhang'])
         result['num_samples_with_this_junction'] = result[batch_columns['num_samples_with_this_junction']].sum(axis=1).astype(COLUMN_TYPES['num_samples_with_this_junction'])
+        result['num_samples_total'] = result[batch_columns['num_samples_total']].sum(axis=1).astype(COLUMN_TYPES['num_samples_total'])
         result['strand_counter'] = result[batch_columns['strand_counter']].sum(axis=1).astype(COLUMN_TYPES['strand_counter'])
 
         for column in column_names:
             if batch_number > 0: batch_columns[column].remove(column)
-            if args.save_read_counts and column in ['unique_reads', 'multi_mapped_reads']:
+            if args.save_parquet_files and column in ['unique_reads', 'multi_mapped_reads']:
                 output_file_name = f"{column}.batch_{batch_number}.{args.batch_size}_samples.SJ.out.parquet"
-                if args.save_read_counts:
-                    parquet_files[column].append(output_file_name)
-
                 read_count_df = result[batch_columns[column]].astype('float32')
                 write_to_parquet(read_count_df, output_file_name)
+                parquet_file_paths[column].append(output_file_name)
 
-            result.drop(columns=batch_columns[column], inplace=True)
+        result.drop(columns=batch_columns[column], inplace=True)
 
         print_memory_stats(f'after table {i}')
         logging.info(f"Done processing batch {batch_number}")
@@ -177,28 +187,31 @@ def main():
     #pd.set_option('display.max_columns', 10000)
     #logging.info(result.describe())
 
-
     if args.output_path:
         output_path = args.output_path
     else:
         output_path = f"combined.{len(args.paths)}_samples"
         if args.normalize_read_counts:
             output_path += ".normalized"
-        output_path += ".SJ.out.parquet"
+        output_path += ".SJ.out"
 
-    write_to_parquet(result, output_path)
+    df.to_csv(f"{output_path}.tsv", index=False, sep="\t")
+    logging.info(f"Wrote out {output_path}.tsv")
 
-    if args.save_read_counts:
+    #write_to_parquet(result, output_path)
+
+    # combine batch parquet files into single parquet output file
+    if args.save_parquet_files:
         logging.info("Combine parquet files: ")
         for column in ['unique_reads', 'multi_mapped_reads']:
             tables = []
-            for path in parquet_files[column]:
+            for path in parquet_file_paths[column]:
                 tables.append(pd.read_parquet(path))
             result = pd.DataFrame().join(tables, how="outer")
             result.fillna(0, inplace=True)
             result = result.astype('float32')
 
-            write_to_parquet(result, os.path.join(os.path.dirname(output_path), f"{column}.{os.path.basename(output_path)}"))
+            write_to_parquet(result, os.path.join(os.path.dirname(output_path), f"{column}.{os.path.basename(output_path)}.parquet"))
 
 
 if __name__ == "__main__":
