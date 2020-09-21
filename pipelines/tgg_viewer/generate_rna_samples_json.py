b514861a5343ae87f2d3ab83202da512a6e8baab
bw2 <weisburd@broadinistitute.org>
2020-09-21 02:53:07 -0400

    .

--- pipelines/tgg_viewer/generate_rna_samples_json.py
+++ pipelines/tgg_viewer/generate_rna_samples_json.py
@@ -180,57 +180,20 @@ for batch_name, rows in rows_by_batch.items():
                         ]
                     },
                     {
-                        "name": "GTEx Blood",
-                        "description": "All splice junctions from all 755 GTEx v3 blood samples. The junction-spanning read counts and read coverage are summed across all samples.",
-                        "data": [
-                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_blood.755_samples.bigWig" },
-                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_blood.755_samples.junctions.bed.gz" }
-                        ]
-                    },
-                    {
-                        "name": "GTEx Fibs",
-                        "description": "All splice junctions from all 504 GTEx v3 fibroblast samples. The junction-spanning read counts and read coverage are summed across all samples.",
-                        "data": [
-                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_fibs.504_samples.bigWig" },
-                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_fibs.504_samples.junctions.bed.gz" }
-                        ]
-                    },
-                    {
-                        "name": "GTEx Lymph",
-                        "description": "All splice junctions from all 174 GTEx v3 lymphocyte samples.<br/>The junction-spanning read counts and read coverage are summed across all samples.",
-                        "data": [
-                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_lymphocytes.174_samples.bigWig" },
-                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_lymphocytes.174_samples.junctions.bed.gz" }
-                        ]
-                    },
-                    {
-                        "name": "GTEx Brain: Cortex",
-                        "description": "All splice junctions from all 255 GTEx v3 cortex samples.<br/>The junction-spanning read counts and read coverage are summed across all samples.",
-                        "data": [
-                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_brain_cortex.255_samples.bigWig" },
-                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_brain_cortex.255_samples.junctions.bed.gz" }
-                        ]
-                    },
-                    {
-                        "name": "GTEx Brain: Front. Cortex",
-                        "description": "All splice junctions from all 209 GTEx v3 frontal cortex samples.<br/>The junction-spanning read counts and read coverage are summed across all samples.",
-                        "data": [
-                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_frontal_cortex.209_samples.bigWig" },
-                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_frontal_cortex.209_samples.junctions.bed.gz" }
-                        ]
-                    }
-                ]
-            },
-            {
-                "categoryName": "GTEx Tracks (Norm.)",
-                "rows": [
-            
-                    {
                         "name": "GTEx Muscle (Norm.)",
                         "description": "Highly expressed junctions from all 803 GTEx v3 muscle samples.<br/>The junction-spanning read counts are normalized to represent the average spanning read count per-sample (see formula below). Only junctions with rounded normalized spanning read count > 0 are included in this track.<br /><br />average_unique_reads_per_muscle_sample = (total_unqiue_reads_in_all_muscle_samples / number_of_muscle_samples)<br />per_sample_normalized_read_count = raw_read_count * average_unique_reads_per_muscle_sample / total_unqiue_reads_in_this_sample<br/>normalized read count for junction = sum(per_sample_normalized_read_counts) / number_of_muscle_samples<br/>",
                         "data": [
                             { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_muscle.803_samples.bigWig" },
                             { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_muscle.803_samples.normalized.junctions.bed.gz" }
+                        ]
+                    },
+                                    
+                    {
+                        "name": "GTEx Blood",
+                        "description": "All splice junctions from all 755 GTEx v3 blood samples. The junction-spanning read counts and read coverage are summed across all samples.",
+                        "data": [
+                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_blood.755_samples.bigWig" },
+                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_blood.755_samples.junctions.bed.gz" }
                         ]
                     },
                     {
@@ -241,12 +204,30 @@ for batch_name, rows in rows_by_batch.items():
                             { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_blood.755_samples.normalized.junctions.bed.gz" }
                         ]
                     },
+                    
+                    {
+                        "name": "GTEx Fibs",
+                        "description": "All splice junctions from all 504 GTEx v3 fibroblast samples. The junction-spanning read counts and read coverage are summed across all samples.",
+                        "data": [
+                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_fibs.504_samples.bigWig" },
+                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_fibs.504_samples.junctions.bed.gz" }
+                        ]
+                    },
                     {
                         "name": "GTEx Fibs (Norm.)",
                         "description": "Highly expressed junctions from all 504 GTEx v3 fibroblast samples.<br/>The junction-spanning read counts are normalized to represent the average spanning read count per-sample (see formula below). Only junctions with rounded normalized spanning read count > 0 are included in this track.<br /><br />average_unique_reads_per_fibs_sample = (total_unqiue_reads_in_all_fibs_samples / number_of_fibs_samples)<br />per_sample_normalized_read_count = raw_read_count * average_unique_reads_per_fibs_sample / total_unqiue_reads_in_this_sample<br/>normalized read count for junction = sum(per_sample_normalized_read_counts) / number_of_fibs_samples<br/>",
                         "data": [
                             { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_fibs.504_samples.bigWig" },
                             { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_fibs.504_samples.normalized.junctions.bed.gz" }
+                        ]
+                    },
+                    
+                    {
+                        "name": "GTEx Lymph",
+                        "description": "All splice junctions from all 174 GTEx v3 lymphocyte samples.<br/>The junction-spanning read counts and read coverage are summed across all samples.",
+                        "data": [
+                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_lymphocytes.174_samples.bigWig" },
+                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_lymphocytes.174_samples.junctions.bed.gz" }
                         ]
                     },
                     {
@@ -257,12 +238,30 @@ for batch_name, rows in rows_by_batch.items():
                             { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_lymphocytes.174_samples.normalized.junctions.bed.gz" }
                         ]
                     },
+                    
+                    {
+                        "name": "GTEx Brain: Cortex",
+                        "description": "All splice junctions from all 255 GTEx v3 cortex samples.<br/>The junction-spanning read counts and read coverage are summed across all samples.",
+                        "data": [
+                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_brain_cortex.255_samples.bigWig" },
+                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_brain_cortex.255_samples.junctions.bed.gz" }
+                        ]
+                    },
                     {
                         "name": "GTEx Brain: Cortex (Norm.)",
                         "description": "Highly expressed junctions from all 255 GTEx v3 brain cortex samples.<br />The junction-spanning read counts are normalized to represent the average spanning read count per-sample (see formula below). Only junctions with rounded normalized spanning read count > 0 are included in this track.<br /><br />average_unique_reads_per_cortex_sample = (total_unqiue_reads_in_all_cortex_samples / number_of_cortex_samples)<br />per_sample_normalized_read_count = raw_read_count * average_unique_reads_per_cortex_sample / total_unqiue_reads_in_this_sample<br/>normalized read count for junction = sum(per_sample_normalized_read_counts) / number_of_cortex_samples<br/>",
                         "data": [
                             { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_brain_cortex.255_samples.bigWig" },
                             { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_brain_cortex.255_samples.normalized.junctions.bed.gz" }
+                        ]
+                    },
+                    
+                    {
+                        "name": "GTEx Brain: Front. Cortex",
+                        "description": "All splice junctions from all 209 GTEx v3 frontal cortex samples.<br/>The junction-spanning read counts and read coverage are summed across all samples.",
+                        "data": [
+                            { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_frontal_cortex.209_samples.bigWig" },
+                            { "type": "junctions", "url": "gs://tgg-viewer/ref/GRCh38/gtex_v8/GTEX_frontal_cortex.209_samples.junctions.bed.gz" }
                         ]
                     },
                     {
