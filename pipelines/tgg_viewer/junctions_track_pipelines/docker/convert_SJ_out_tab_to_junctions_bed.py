5e4c1c66e92aa7380e92ac775759a7cbef6a3e74
bw2 <weisburd@broadinistitute.org>
2020-10-28 19:13:22 -0400

    allow more than one input file

--- pipelines/tgg_viewer/junctions_track_pipelines/docker/convert_SJ_out_tab_to_junctions_bed.py
+++ pipelines/tgg_viewer/junctions_track_pipelines/docker/convert_SJ_out_tab_to_junctions_bed.py
@@ -13,7 +13,9 @@ p.add_argument('-g', '--gencode-gff', help="Path of gencode .gff3 file for annot
     "This is needed because STAR in 2-pass mode marks junctions as 'known' when they were found on the 1st pass, "
     "which represents almost all junctions.")
 p.add_argument('-L', '--interval', action="append", help="Only keep junctions contained in the given chr:start-end interval(s)")
-p.add_argument('input_path', help="Input *.SJ.out.tab(.gz?) or .tsv(.gz?) file path. A header line with column labels is optional.")
+p.add_argument('input_path', nargs="+", help="Input *.SJ.out.tab(.gz?) or .tsv(.gz?) file path. "
+    "A header line with column labels is optional. If more than 1 input path is specified, they will be converted  "
+    "one after the other, in a loop.")
 args = p.parse_args()
 
 print(f"Input: {args.input_path}")
@@ -52,17 +54,6 @@ chr7    127474697  127475864  Pos4  0  +
 chr7    127475864  127477031  Neg1  0  -
 """
 
-suffix = ".junctions.bed"
-if args.interval:
-    if len(args.interval) <= 3:
-        suffix = "." + "__".join([i.replace(",", "").replace(":", "-") for i in args.interval]) + suffix
-    else:
-        suffix = ".filtered" + suffix
-else:
-    args.interval = []
-
-output_path = re.sub("(.SJ.out)?.tab(.gz)?$", "", args.input_path) + suffix
-
 STRAND_LOOKUP = {
     '0': '.',
     '1': '+',
@@ -92,77 +83,90 @@ def parse_interval(i):
 intervals = [parse_interval(i) for i in args.interval]
 gencode_v26_introns_set = parse_gencode_gff(args.gencode_gff)
 
-counter = 0
-annotated_counter = 0
-with (gzip.open if args.input_path.endswith("gz") else open)(args.input_path, "rt") as f, open(output_path, "wt") as bed_file:
-    header = None
-    for i, line in enumerate(f):
-        if i == 0 and "chrom" in line.lower():
-            header = line.strip("\n").split("\t")
-            continue # skip header
+for input_path in args.input_path:
 
-        fields = line.strip("\n").split("\t")
-        chrom = fields[0]
-        start_1based = int(fields[1])
-        end_1based = int(fields[2])
+    suffix = ".junctions.bed"
+    if args.interval:
+        if len(args.interval) <= 3:
+            suffix = "." + "__".join([i.replace(",", "").replace(":", "-") for i in args.interval]) + suffix
+        else:
+            suffix = ".filtered" + suffix
+    else:
+        args.interval = []
 
-        if intervals:
-            for interval_chrom, interval_start, interval_end in intervals:
-                if interval_chrom == chrom and start_1based >= interval_start and end_1based <= interval_end:
-                    break
+    output_path = re.sub("(.SJ.out)?.tab(.gz)?$", "", input_path) + suffix
+
+    counter = 0
+    annotated_counter = 0
+    with (gzip.open if input_path.endswith("gz") else open)(input_path, "rt") as f, open(output_path, "wt") as bed_file:
+        header = None
+        for i, line in enumerate(f):
+            if i == 0 and "chrom" in line.lower():
+                header = line.strip("\n").split("\t")
+                continue # skip header
+
+            fields = line.strip("\n").split("\t")
+            chrom = fields[0]
+            start_1based = int(fields[1])
+            end_1based = int(fields[2])
+
+            if intervals:
+                for interval_chrom, interval_start, interval_end in intervals:
+                    if interval_chrom == chrom and start_1based >= interval_start and end_1based <= interval_end:
+                        break
+                else:
+                    continue
+
+
+            strand = STRAND_LOOKUP[fields[3]]
+            intron_motif = MOTIF_LOOKUP[fields[4]]
+            #is_annotated = str(bool(int(fields[5]))).lower()
+            num_uniquely_mapped_reads = int(round(float(fields[6])))
+            num_multi_mapped_reads = int(round(float(fields[7])))
+            maximum_spliced_alignment_overhang = int(fields[8])
+
+            key = (chrom, start_1based, end_1based)
+            if key in gencode_v26_introns_set:
+                is_annotated = "true"
+                annotated_counter += 1
             else:
+                is_annotated = "false"
+
+            if num_uniquely_mapped_reads + num_multi_mapped_reads == 0:
+                # skip junctions with no read support. Rounding down to 0 may result in this.
                 continue
 
+            output_fields = [
+                f"motif={intron_motif}",
+                f"uniquely_mapped={num_uniquely_mapped_reads}",
+                f"multi_mapped={num_multi_mapped_reads}",
+                f"maximum_spliced_alignment_overhang={maximum_spliced_alignment_overhang}",
+                f"annotated_junction={is_annotated}",
+            ]
 
-        strand = STRAND_LOOKUP[fields[3]]
-        intron_motif = MOTIF_LOOKUP[fields[4]]
-        #is_annotated = str(bool(int(fields[5]))).lower()
-        num_uniquely_mapped_reads = int(round(float(fields[6])))
-        num_multi_mapped_reads = int(round(float(fields[7])))
-        maximum_spliced_alignment_overhang = int(fields[8])
+            if header and 'num_samples_with_this_junction' in header and 'num_samples_total' in header:
+                idx = header.index('num_samples_with_this_junction')
+                output_fields.append(f"num_samples_with_this_junction={int(fields[idx])}")
 
-        key = (chrom, start_1based, end_1based)
-        if key in gencode_v26_introns_set:
-            is_annotated = "true"
-            annotated_counter += 1
-        else:
-            is_annotated = "false"
+                idx = header.index('num_samples_total')
+                output_fields.append(f"num_samples_total={int(fields[idx])}")
 
-        if num_uniquely_mapped_reads + num_multi_mapped_reads == 0:
-            # skip junctions with no read support. Rounding down to 0 may result in this.
-            continue
+            gffTags = ";".join(output_fields)
 
-        output_fields = [
-            f"motif={intron_motif}",
-            f"uniquely_mapped={num_uniquely_mapped_reads}",
-            f"multi_mapped={num_multi_mapped_reads}",
-            f"maximum_spliced_alignment_overhang={maximum_spliced_alignment_overhang}",
-            f"annotated_junction={is_annotated}",
-        ]
+            score = num_uniquely_mapped_reads
 
-        if header and 'num_samples_with_this_junction' in header and 'num_samples_total' in header:
-            idx = header.index('num_samples_with_this_junction')
-            output_fields.append(f"num_samples_with_this_junction={int(fields[idx])}")
+            counter += 1
+            bed_file.write("\t".join(map(str, [
+                chrom,
+                start_1based - 1,
+                end_1based,
+                gffTags,
+                score,
+                strand,
+            ])) + "\n")
 
-            idx = header.index('num_samples_total')
-            output_fields.append(f"num_samples_total={int(fields[idx])}")
+    print(f"Wrote {counter} intervals to {output_path}.gz of which {annotated_counter} ({100*annotated_counter/counter:.01f}%) are known introns")
 
-        gffTags = ";".join(output_fields)
-
-        score = num_uniquely_mapped_reads
-
-        counter += 1
-        bed_file.write("\t".join(map(str, [
-            chrom,
-            start_1based - 1,
-            end_1based,
-            gffTags,
-            score,
-            strand,
-        ])) + "\n")
-
-print(f"Wrote {counter} intervals to {output_path}.gz of which {annotated_counter} ({100*annotated_counter/counter:.01f}%) are known introns")
-
-os.system(f"bgzip -f {output_path}")
-os.system(f"tabix {output_path}.gz")
+    os.system(f"bgzip -f {output_path}")
+    os.system(f"tabix {output_path}.gz")
 
