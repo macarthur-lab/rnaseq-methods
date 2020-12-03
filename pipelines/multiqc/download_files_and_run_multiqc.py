1c5245e11b31ec55f922122eda2f49e0461896dd
bw2 <weisburd@broadinistitute.org>
2020-12-02 22:13:56 -0500

    multiqc

--- pipelines/multiqc/download_files_and_run_multiqc.py
+++ pipelines/multiqc/download_files_and_run_multiqc.py
@@ -15,7 +15,8 @@ logger.setLevel(logging.INFO)
 def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument("--clean", action="store_true", help="Delete previous files")
-    p.add_argument("batch_name", default="all", choices=["all", "batch_0", "batch_1_muntoni", "batch_2020_04", "batch_2020_08", "batch_2020_08__walsh"])
+    p.add_argument("--dont-download", action="store_true", help="Skip the download files step")
+    p.add_argument("batch_name", nargs="+", default=["all"], choices=["all", "batch_0", "batch_1_muntoni", "batch_2020_04", "batch_2020_08", "batch_2020_08__walsh"])
     args = p.parse_args()
 
     return args
@@ -41,42 +42,46 @@ def main():
     args = parse_args()
     logger.info("Args: " + pprint.pformat(args.__dict__))
 
-    original_dir = os.path.abspath(os.getcwd())
-    source_prefix = "gs://macarthurlab-rnaseq/" + (args.batch_name if args.batch_name != "all" else "*")
-    dest_prefix = os.path.join(original_dir, "data/")
-    if not os.path.isdir(dest_prefix):
-        logger.error(dest_prefix + " dir not found.")
-        sys.exit(1)
+    for batch_name in args.batch_name:
+        original_dir = os.path.abspath(os.getcwd())
+        source_prefix = "gs://macarthurlab-rnaseq/" + (batch_name if batch_name != "all" else "*")
+        dest_prefix = os.path.join(original_dir, "data/")
+        if not os.path.isdir(dest_prefix):
+            logger.error(dest_prefix + " dir not found.")
+            sys.exit(1)
 
-    dest_prefix = os.path.join(dest_prefix, args.batch_name)
+        dest_prefix = os.path.join(dest_prefix, batch_name)
+        
+        if args.clean:
+            for subdirs in ["star", "rna_seqc", "fastqc"]:
+                run("rm -r %s" % os.path.join(dest_prefix, subdirs))
 
-    if args.clean:
-        for subdirs in ["star", "rna_seqc", "fastqc"]:
-            run("rm -r %s" % os.path.join(dest_prefix, subdirs))
+        if not args.dont_download:
+            # star
+            gsutil_cp("%s/star/*.Log.final.out" % source_prefix, dest_prefix+"/star/")
+            gsutil_cp("%s/star/*.ReadsPerGene.out.tab.gz" % source_prefix,  dest_prefix+"/star/genecounts/")
 
-    # star
-    gsutil_cp("%s/star/*.Log.final.out" % source_prefix, dest_prefix+"/star/")
-    gsutil_cp("%s/star/*.ReadsPerGene.out.tab.gz" % source_prefix,  dest_prefix+"/star/genecounts/")
+            # rnaseqc
+            gsutil_cp("%s/rnaseqc/*.metrics.tsv" % source_prefix,  dest_prefix+"/rna_seqc/metrics/")
 
-    # rnaseqc
-    gsutil_cp("%s/rnaseqc/*.metrics.tsv" % source_prefix,  dest_prefix+"/rna_seqc/metrics/")
+            # fastqc
+            gsutil_cp("%s/fastqc/zip/*_fastqc.zip" % source_prefix,  dest_prefix+"/fastqc/zip/")
+            
+            # unzip
+            run("gunzip -f " + dest_prefix+"/star/genecounts/*.gz")
+            chdir(dest_prefix+"/fastqc/zip/")
+            for zip_path in glob.glob("*.zip"):
+                run("unzip -o " + zip_path)
 
-    # fastqc
-    gsutil_cp("%s/fastqc/zip/*_fastqc.zip" % source_prefix,  dest_prefix+"/fastqc/zip/")
+        # run multiqc
+        chdir(dest_prefix)
+        run("python3 -m multiqc -f -m star -m fastqc -m rna_seqc --filename %s.html ." % batch_name)
 
-    # unzip
-    run("gunzip -f " + dest_prefix+"/star/genecounts/*.gz")
-    chdir(dest_prefix+"/fastqc/zip/")
-    for zip_path in glob.glob("*.zip"):
-        run("unzip -o " + zip_path)
+        run("rm -r %s" % os.path.join(original_dir, "%s_data" % batch_name))
+        run("mv -f %s* %s" % (batch_name, original_dir))
+        logger.info("Done")
 
-    # run multiqc
-    chdir(dest_prefix)
-    run("python3 -m multiqc -f -m star -m fastqc -m rna_seqc --filename %s.html ." % args.batch_name)
-
-    run("rm -r %s" % os.path.join(original_dir, "%s_data" % args.batch_name))
-    run("mv -f %s* %s" % (args.batch_name, original_dir))
-    logger.info("Done")
+        chdir(original_dir)
 
 
 if __name__ == "__main__":
