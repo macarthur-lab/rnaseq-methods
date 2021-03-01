0a1088c69836f1c84951a33d14802db0181b8553
bw2 <weisburd@broadinistitute.org>
2021-03-01 11:56:53 -0500

    add segdups

--- pipelines/tgg_viewer/update_rna_sample_config_json.py
+++ pipelines/tgg_viewer/update_rna_sample_config_json.py
@@ -371,6 +371,13 @@ for batch_name, rows in rows_by_batch.items():
                         "data": [
                             { "type": "coverage", "url": "gs://tgg-viewer/ref/GRCh38/mappability/GRCh38_no_alt_analysis_set_GCA_000001405.15-k100_m2.bw" }
                         ]
+                    },
+                    {
+                        "name": "SegDups >1000 bases",
+                        "description": "Duplications of >1000 Bases of Non-RepeatMasked Sequence downloaded from UCSC",
+                        "data": [
+                            { "type": "gtf", "url": "gs://tgg-viewer/ref/GRCh38/segdups/segdups.gtf.gz" }
+                        ]
                     }
                 ]
             },
