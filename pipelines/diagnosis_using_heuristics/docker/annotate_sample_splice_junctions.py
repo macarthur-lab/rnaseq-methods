034c75c72195e2170be47e016fa85e3123688c61
bw2 <weisburd@broadinistitute.org>
2021-06-09 17:04:45 -0400

    heuristics pipeline

---
+++ pipelines/diagnosis_using_heuristics/docker/annotate_sample_splice_junctions.py
@@ -0,0 +1,15 @@
+
+"""
+TODO
+
+take individual junctions
+
+left-join with:
+    portcullis
+    all 4 GTEx tissues  (# of samples with junction, # of samples total, min, max, median read support per sample)
+    variants
+    OMIM, constraint, gene lists, etc.
+
+"""
+# TODO OUTRIDER data
+# TODO variants from sample
